53b232bbaca10400356b386c29d6781da8878ecd
JustRoniss <ronaldoluizdev@gmail.com>
2024-01-23 12:46:55 -0300

    subindo nodejs + mysql

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'admin',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Ronaldo');`;
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
     res.send('<h1>Just The Ronis here!</h1>');
